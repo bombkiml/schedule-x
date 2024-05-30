8054aa91728c5b5a224857d26af39330cf113830
bombkiml <nattapat.reh@gmail.com>
2024-05-30 13:34:55 +0700

    add calendar support locale th-TH

---
+++ packages/translations/src/locales/th-TH/calendar.ts
@@ -0,0 +1,15 @@
+import { CalendarTranslations } from '../../types/calendar.translations'
+
+export const calendarEnUS: CalendarTranslations = {
+  Today: 'วันนี้',
+  Month: 'เดือน',
+  Week: 'สัปดาห์',
+  Day: 'วัน',
+  events: 'events',
+  event: 'event',
+  'No events': 'No events',
+  'Next period': 'Next period',
+  'Previous period': 'Previous period',
+  to: 'to', // as in 2/1/2020 to 2/2/2020
+  'Full day- and multiple day events': 'Full day- and multiple day events',
+}
